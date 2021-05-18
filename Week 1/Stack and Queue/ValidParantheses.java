ad7474c6d35acb873bd0855cd8b414900c2f9129
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2021-05-18 17:48:02 -0400

    Parantheses easy

--- Week 1/Stack and Queue/ValidParantheses.java
+++ Week 1/Stack and Queue/ValidParantheses.java
@@ -1,17 +1,47 @@
 import java.util.Stack;
-
+//Basic stack implementation. No Tricky conditions
 public class ValidParantheses {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         if (s.length() < 2)
             return false;
-        return true;
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '{' || curr == '(' || curr == '[') {
+                stack.push(curr);
+            } else {
+                switch (curr) {
+                case '}':
+                    if (stack.peek() == '{')
+                        stack.pop();
+                    else
+                        return false;
+                    break;
+                case ')':
+                    if (stack.peek() == '(')
+                        stack.pop();
+                    else
+                        return false;
+                    break;
+                case ']':
+                    if (stack.peek() == '[')
+                        stack.pop();
+                    else
+                        return false;
+                    break;
+                default:
+                    return false;
+
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
         ValidParantheses obj = new ValidParantheses();
-        obj.isValid("(");
-        System.out.println(obj.isValid(")"));
-        
+        String test = "()[]{}";
+        System.out.println(obj.isValid(test));
+
     }
 }
